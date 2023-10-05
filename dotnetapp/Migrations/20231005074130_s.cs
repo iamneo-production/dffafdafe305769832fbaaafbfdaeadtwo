d815642ed3b671e5cd04c455e7e9209b6d1961f1
Iamneo <techadmin@iamneo.ai>
2023-10-05 07:43:01 +0000

    Backup Commit

---
+++ dotnetapp/Migrations/20231005074130_s.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportsAcademyJobHiring.Migrations
+{
+    public partial class s : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "JobPositions",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "JobPositions",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Department",
+                table: "JobPositions",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "JobApplications",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplicantName",
+                table: "JobApplications",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobApplications_JobPositionId",
+                table: "JobApplications",
+                column: "JobPositionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_JobApplications_JobPositions_JobPositionId",
+                table: "JobApplications",
+                column: "JobPositionId",
+                principalTable: "JobPositions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_JobApplications_JobPositions_JobPositionId",
+                table: "JobApplications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_JobApplications_JobPositionId",
+                table: "JobApplications");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "JobPositions",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "JobPositions",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Department",
+                table: "JobPositions",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "JobApplications",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplicantName",
+                table: "JobApplications",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
